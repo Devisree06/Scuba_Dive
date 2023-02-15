ea71bcda5dd70007bb812fb0593c7e0fc11213bf
Devisree Jayaraman <devisreej@maveric-systems.com>
2023-02-15 13:08:05 +0530

    commit message

--- src/test/java/firstScubaDive/loginPage.java
+++ src/test/java/firstScubaDive/loginPage.java
@@ -1,6 +1,7 @@
 package firstScubaDive;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -9,13 +10,17 @@ import org.openqa.selenium.interactions.Actions;
 public class loginPage {
 	public static void main(String[] args) throws InterruptedException {
 		ChromeDriver driver = new ChromeDriver();
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("disable-notification");
 		driver.manage().window().maximize();
 		driver.get("https://www.ajio.com");
-		WebElement a=driver.findElement(By.xpath("//a[text()='WOMEN']"));
-		Actions action=new Actions(driver);
-		action.moveToElement(a);
+		Thread.sleep(3000);
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("disable-notification");
+		WebElement searchButton=driver.findElement(By.xpath("//input[@placeholder='Search AJIO']"));
+		searchButton.sendKeys("Kids dress",Keys.ENTER);
+		//WebElement a=driver.findElement(By.xpath("//a[text()='WOMEN']"));
+		//a.click();
+		//Actions action=new Actions(driver);
+		//action.moveToElement(a);
 		//driver.findElement(By.xpath("//input[@id='username']")).sendKeys("Devisree");
 		Thread.sleep(1000);
 		driver.quit();
